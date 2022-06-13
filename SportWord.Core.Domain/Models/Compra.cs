7f91e60f24894eb4870aed81ff79e084a2ddb4bf
FernandoLoenardoOrtegaMillon1 <millonleonardo@gmail.com>
2022-06-12 19:26:33 -0600

    Se agrega el primer avance del proyecto

---
+++ SportWord.Core.Domain/Models/Compra.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportWord.Core.Domain.Models
+{
+     public class Compra
+    {
+        public Guid compra_id { get; set; }
+        public DateTime Fecha_compra { get; set; }
+     
+     
+        public User Users { get; set; }
+
+    }
+}
---
+++ SportWord.Core.Domain/Models/Promocion.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SportWord.Core.Domain.Models
+{
+     public class Promocion
+    {
+        public Guid promocion_id { get; set; }
+        public string titulo { get; set; }
+
+       
+        public List <Productos> Productos { get; set; }
+       
+
+    }
+}
